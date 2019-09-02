be58533dd86860ee11e4557c2cfeb79c7f570a6d
MauLara <thebestcarsubaru@gmail.com>
2019-09-02 17:01:26 -0500

    Se_agrego_los_cambios_de_app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'app',
+    component: AppComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HomeComponent,
+    ContactComponent
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
